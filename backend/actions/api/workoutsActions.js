bd050902198e482e01cf613eb6dc7b27e663d3f0
Patryk Siechodnik <p.siechodnik@gmail.com>
2023-03-09 21:33:50 +0100

    backend signup & singin api

--- backend/actions/api/workoutsActions.js
+++ backend/actions/api/workoutsActions.js
@@ -6,7 +6,7 @@ class WorkoutsActions {
     try {
       data = await Workout.find({});
     } catch (error) {
-      return res.status(200).send(error.message);
+      return res.status(400).send(error.message);
     }
     res.status(200).json(data);
   }
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,28 +3,22 @@ const router = express.Router();
 
 const workoutsActions = require("../actions/api/workoutsActions");
 const trainingPlanActions = require("../actions/api/trainingPlanActions");
+const userActions = require("../actions/api/userActions");
 
+//excercises plan actions
 router.get("/all", workoutsActions.showWorkouts);
-
-// router.get("/test", workoutsActions.showTests);
-
-// //pobieranie konkretnej notatki
-// router.get("/notes/:id", noteActions.getNote);
-
-// //post - zapisywanie nowych danych
 router.post("/add", workoutsActions.addWorkout);
-
-// //put - edycja danych
-// router.put("/notes/:id", noteActions.updateNote);
-
-// //delete - usunięcie danych
 router.delete("/excersise/:id", workoutsActions.deleteWorkout);
 
+//training plan actions
 router.get("/training-plans", trainingPlanActions.showPlans);
 router.get("/training-plan/:id", trainingPlanActions.getPlanById);
 router.post("/training-plan", trainingPlanActions.addPlan);
-
 router.put("/edit-plan", trainingPlanActions.editWorkoutInPlan);
 router.get("/edit-plan", trainingPlanActions.findExcById);
+
+//users actions
+router.post("/user", userActions.registerUser);
+router.get("/user", userActions.loginUser)
 
 module.exports = router;
